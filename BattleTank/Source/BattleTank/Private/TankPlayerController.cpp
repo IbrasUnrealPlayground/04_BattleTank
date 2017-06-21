eb9d9f8f5de005dcee9d9a4853a34473f34a9847
DESKTOP-9NUUM9N\m <ihindawi@gmail.com>
2017-06-21 12:03:40 +0300

    Virtual overriding
    Learning to virtually override some functions !

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,8 +4,25 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Player Controller Begin Play"))
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller posessing: %s"), *ControlledTank->GetName())
+	}
+}
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+
 }
